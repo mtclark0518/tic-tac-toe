3216fe579a14410198056b99b9558c1669e19c0b
mtclark0518 <mtclark0518@gmail.com>
2017-07-08 00:04:44 -0600

    gets somewhere with game logic

--- app.js
+++ app.js
@@ -75,41 +75,80 @@
 
 
 
-var gameBoard = [];
 
 
-var Squares = function(id, index, inPlay) {
-	this.id = id;
-	this.index = index;
-	this.inPlay =  inPlay;
-	gameBoard.push(this);
-};
 
-
-var Player = function(index, team, myTurn, myMoves){
-	this.index = index;
-	this.team = team;
-	this.myMoves = [];
+var gameBoard = document.querySelector('table');
+//SCORES
 	
-};
-Player.prototype = {
+var total = 0;
+
+var game = {
+
+	addMove : function(){
+		total += 1;
+		console.log('clickity');
+	},
 	whoseTurn : function(){
-		if(this.index === 0)
-			{console.log('its myturn');}
+		if(total % 2 === 0) {
+			lebron.myTurn = true;
+			steph.myTurn = false;
+			console.log('lebron');
+		}else if(total % 2 === 1) {
+			lebron.myTurn = false;
+			steph.myTurn = true;
+			console.log('steph');
+		}
 	}
 };
-var p1 = new Player(0, "X");
-console.log(p1);
 
-var tl = new Squares('tl', 0, false);
-var tm = new Squares('tm', 1, false);
-var tr = new Squares('tr', 2, false);
+var Squares = function(id, index) {
+	this.id = id;
+	this.index = index;
+	this.selected = document.querySelector('#' + id);
+	this.selected.addEventListener('click', game.addMove);
+
+};
+
+Squares.prototype = {
+	inPlay : false
+};
+
+var Player = function(index, team, myTurn, src, myMoves){
+	this.index = index;
+	this.team = team;
+	this.myTurn = myTurn;
+	this.src = src;
+	this.myMoves = myMoves || [];
+	
+};
+
+Player.prototype = {
+
+};
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', newGame);
 
 
+function newGame(){
+	total = 0;
+	console.log('a new game is starting');
+	var lebron = new Player(0, "X", true, "img/lebron.png");
+	var steph = new Player(1, "O", false, "img/steph.png");
+	var tl = new Squares('tl', 0);
+	var tm = new Squares('tm', 1);
+	var tr = new Squares('tr', 2);
+	var ml = new Squares('ml', 3);
+	var mm = new Squares('mm', 4);
+	var mr = new Squares('mr', 5);
+	var bl = new Squares('bl', 6);
+	var bm = new Squares('bm', 7);
+	var br = new Squares('br', 8);
+}
 
 
-
-
+newGame();
 
 
 
